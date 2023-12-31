8994bbf99d42fd95b373c7a9e91c9eff7cc55e4c
Himanshu Bainwala <himanshubainwala@Himanshus-MBP.domain>
2023-12-31 15:21:25 -0600

    Added Connections

--- app/playlists/[id]/page.tsx
+++ app/playlists/[id]/page.tsx
@@ -4,6 +4,15 @@ import { useSearchParams } from "next/navigation";
 import { Track } from "@/models/Track";
 import axios from "axios";
 import ThreeScene from "@/components/ThreeScene";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 type Props = {
   params: {
@@ -22,6 +31,7 @@ type TracksReturnProps = {
 function page({ params: { id } }: Props) {
   const searchParams = useSearchParams();
   const [tracks, setTracks] = useState<Track[]>();
+  const [position, setPosition] = useState<string>("Artists");
 
   const getTracks = async () => {
     const { data } = await axios.get<TracksReturnProps>(
@@ -52,8 +62,26 @@ function page({ params: { id } }: Props) {
   }, []);
 
   return (
-    <div className="flex justify-center items-center mt-12 flex-col space-y-4">
-      {tracks?.length ? <ThreeScene tracks={tracks} /> : null}
+    <div className="flex flex-col space-y-4 justify-center items-center">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">{position}</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+            <DropdownMenuRadioItem value="Artists">
+              Artists
+            </DropdownMenuRadioItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuRadioItem value="Tracks">Tracks</DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <div className="flex justify-center items-center mt-12 flex-col space-y-4">
+        {tracks?.length ? (
+          <ThreeScene tracks={tracks} dataType={position} />
+        ) : null}
+      </div>
     </div>
   );
 }
--- components/ThreeScene.tsx
+++ components/ThreeScene.tsx
@@ -4,15 +4,121 @@ import React, { useRef, useEffect, useState } from "react";
 import * as THREE from "three";
 import browood_font from "../assets/Browood_Regular.json";
 import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
-import { FontLoader } from "three/addons/loaders/FontLoader.js";
+import { Font, FontLoader } from "three/addons/loaders/FontLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { Track } from "@/models/Track";
 
 type ThreeSceneProps = {
   tracks: Track[];
+  dataType: string;
 };
 
-const ThreeScene: React.FC<ThreeSceneProps> = ({ tracks }: ThreeSceneProps) => {
+const addConnections = (
+  scene: THREE.Scene,
+  dataHashMap: Map<string, THREE.Vector3>,
+  dataNetwork: Set<string>
+) => {
+  dataNetwork.forEach((connection) => {
+    const [artist1, artist2] = connection
+      .split("-")
+      .map((el) => dataHashMap.get(el));
+
+    if (artist1 != undefined && artist2 != undefined) {
+      var tG = new THREE.BufferGeometry().setFromPoints([artist1, artist2]);
+      var tM = new THREE.LineBasicMaterial({ color: "yellow" });
+      var tracker = new THREE.Line(tG, tM);
+      scene.add(tracker);
+    }
+  });
+};
+
+const addTracksToScene = (
+  tracks: Track[],
+  font: Font,
+  scene: THREE.Scene,
+  z: number
+) => {
+  var songIndex = 0;
+  var tracksSet = new Set<string>();
+  for (var track of tracks) {
+    if (tracksSet.has(track.name)) continue;
+    else tracksSet.add(track.name);
+
+    const textGeometry = new TextGeometry(track.name, {
+      font: font,
+      size: 0.05,
+      height: 0.005,
+    });
+
+    const material = new THREE.MeshBasicMaterial({
+      color: Math.random() * 0xffffff,
+    });
+    const row = Math.floor(Math.sqrt(5));
+    const x = (songIndex % row) * 3 - (row - 1);
+    z = Math.floor(songIndex / row) * 3 - (row - 1);
+
+    const text = new THREE.Mesh(textGeometry, material);
+    text.position.set(x * 0.5, 0, z);
+    scene.add(text);
+    songIndex = songIndex + 1;
+  }
+  return z;
+};
+
+const addArtistsToScene = (
+  tracks: Track[],
+  font: Font,
+  scene: THREE.Scene,
+  z: number
+) => {
+  var songIndex = 0;
+  var artistsSet = new Set<string>();
+  var artistsHashMap = new Map<string, THREE.Vector3>();
+  var artistsNetwork = new Set<string>();
+
+  for (var track of tracks) {
+    for (var artist of track.artists) {
+      if (artistsSet.has(artist.name)) continue;
+      else artistsSet.add(artist.name);
+
+      const textGeometry = new TextGeometry(artist.name, {
+        font: font,
+        size: 0.05,
+        height: 0.005,
+      });
+
+      const material = new THREE.MeshBasicMaterial({
+        color: Math.random() * 0xffffff,
+      });
+      const row = Math.floor(Math.sqrt(5));
+      const x = (songIndex % row) * 3 - (row - 1);
+      z = Math.floor(songIndex / row) * 3 - (row - 1);
+
+      const text = new THREE.Mesh(textGeometry, material);
+      text.position.set(x * 0.5, 0, z);
+      scene.add(text);
+      artistsHashMap.set(artist.name, new THREE.Vector3(x * 0.5, 0, z));
+      songIndex = songIndex + 1;
+
+      for (var artist2 of track.artists) {
+        if (artist.name === artist2.name) continue;
+        if (
+          !artistsNetwork.has(artist.name + "-" + artist2.name) &&
+          !artistsNetwork.has(artist2.name + "-" + artist.name)
+        ) {
+          artistsNetwork.add(artist.name + "-" + artist2.name);
+        }
+      }
+    }
+  }
+  addConnections(scene, artistsHashMap, artistsNetwork);
+  return z;
+};
+
+const ThreeScene: React.FC<ThreeSceneProps> = ({
+  tracks,
+  dataType,
+}: ThreeSceneProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
   var camera: THREE.PerspectiveCamera;
   var renderer: THREE.WebGLRenderer;
@@ -35,27 +141,14 @@ const ThreeScene: React.FC<ThreeSceneProps> = ({ tracks }: ThreeSceneProps) => {
     const moveSpeed = 0.5;
 
     const font = new FontLoader().parse(browood_font);
-    const material = new THREE.MeshBasicMaterial({ color: "green" });
-    var songIndex = 0;
     var z = 0;
 
-    for (var track of tracks) {
-      const textGeometry = new TextGeometry(track.name, {
-        font: font,
-        size: 0.05,
-        height: 0.005,
-      });
+    z =
+      dataType == "Tracks"
+        ? addTracksToScene(tracks, font, scene, z)
+        : addArtistsToScene(tracks, font, scene, z);
 
-      const row = Math.floor(Math.sqrt(5));
-      const x = (songIndex % row) * 3 - (row - 1);
-      z = Math.floor(songIndex / row) * 3 - (row - 1);
-
-      const text = new THREE.Mesh(textGeometry, material);
-      text.position.set(x * 0.5, 0, z);
-      scene.add(text);
-      songIndex = songIndex + 1;
-    }
-    const targetPosition = new THREE.Vector3(0.45, 0, z);
+    const targetPosition = new THREE.Vector3(0.45, 2, z);
     renderer.render(scene, camera);
 
     function onKeyDown(event: KeyboardEvent) {
@@ -90,6 +183,7 @@ const ThreeScene: React.FC<ThreeSceneProps> = ({ tracks }: ThreeSceneProps) => {
       camera.position.lerp(targetPosition, 0.1);
       renderer.render(scene, camera);
     }
+
     animate();
   };
 
